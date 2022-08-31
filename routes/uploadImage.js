849e4cb7cfadd29249dcf7db58df7f7b18583ffd
bilel.elmarzouki <bilel.elmarzouki>
2022-08-31 08:56:06 +0100

    modif upload image

--- routes/uploadImage.js
+++ routes/uploadImage.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cloudinary = require("../utils/cloudinary");
 const upload = require("../utils/multer");
 const router = express.Router();
+
+
+
 router.post("/", upload.single("image"), async (req, res) => {
-  /*   console.log("hhhh",req.body) */
+    console.log("hhhhyyyyyyyyyyyyy",req.body) 
   try {
-    /* console.log("hhhh",req.body)  */
+    
     const result = await cloudinary.uploader.upload(req.body.image);
     cloudinary.video("dog", {controls:true, transformation: [
         {width: 0.4, angle: 20},
@@ -16,4 +19,45 @@ router.post("/", upload.single("image"), async (req, res) => {
     console.log(err);
   }
 });
+
+
+router.post("/uploadimage", upload.single("image"), async (req, res) => {
+
+  try {
+
+    // Upload image to cloudinary
+
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+ 
+
+    // Create new user
+
+    // let user = new User({
+
+    //   name: req.body.name,
+
+    //   avatar: result.secure_url,
+
+    //   cloudinary_id: result.public_id,
+
+    // });
+
+    // Save user
+
+    // await user.save();
+
+    res.json(result.secure_url);
+
+  } catch (err) {
+
+    console.log(err);
+
+  }
+
+});
+
+
+
+
 module.exports = router;
